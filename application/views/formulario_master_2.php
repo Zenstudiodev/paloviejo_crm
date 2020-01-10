4380d869b3a65758726833cb2cbe73f7d0d20991
Latios-ws <csamayoa@zenstudiogt.com>
2020-01-10 13:19:29 -0600

    form 2

--- application/views/formulario_master_2.php
+++ application/views/formulario_master_2.php
@@ -215,6 +215,7 @@ if ($formulario_2) {
                             </div>
 
                             <?php if($formulario_2_extras){?>
+                            <div class="row" id="extras_row">
                                 <?php
                                 $extra_number = 1;
                                 foreach ($formulario_2_extras->result() as $extra) {  ?>
@@ -235,6 +236,7 @@ if ($formulario_2) {
                                     <input type="hidden" name="extra_fields" id="extra_fields" value="<?php echo $extra_number;?>">
 
                                 <?php } ?>
+                            </div>
                                 <div class="row">
                                     <div class="form-group">
                                         <div class="col-md-3 col-sm-3 col-xs-12 col-md-offset-3">
