96b32019ad9efc2876b4f33eb9ee6f8f28595a74
Latios-ws <csamayoa@zenstudiogt.com>
2020-09-01 13:03:05 -0600

    c

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'https://crm.paloviejosa.com/';
+$config['base_url'] = 'http://crm.zenstudiogt.com/';
 
 
 /*
